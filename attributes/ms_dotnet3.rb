88bc4b5e32fd1b9505f456a9c03934901245fcd0
yeungs <yeung.siu@citrix.com>
2015-06-09 10:03:12 -0400

    Supports .Net3.5 on Windows 8.1 and Server 2012R2
    This only works if we turn on enable all dependent Windows feature on
    Windows 2012.
    
    make the attribute specific to .Net Framework 3.5
    
    Create separate attribute file .Net Framework 3.5 to enable all the
    features.
    
    undo the update
    
    remove blank space
    
    Add a condition on feature enable "/all"
    
    The "/all" is only available in 6.2 (Window 8/Server 2012 or newer).
    Adding a condition to turn set true based on that version.
    
    reverting file. accidentally modified this file.

---
+++ attributes/ms_dotnet3.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: ms_dotnet
+# Attributes:: ms_dotnet3
+# Author:: Jeremy MAURO (<j.mauro@criteo.com>)
+#
+# Copyright (C) 2014 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if platform? 'windows'
+  nt_version = node['platform_version'].to_f
+
+  # DISM /all is only available on NT Version 6.2 (Windows 8/2012) or newer.
+  default['ms_dotnet']['v3']['enable_all_features'] = nt_version >= 6.2
+end
